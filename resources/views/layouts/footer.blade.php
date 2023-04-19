9daed57da1f5385d172fccc34e831ca81677aa06
vivekpatil94 <vsoftinfotech@gmail.com>
2023-04-19 15:28:11 +0530

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Refer. &copy; <a href="https://adminlte.io">AdminLTE.io</a>
+    </div>
+    <strong>Mixed by</b> <a href="https://github.com/KennyChou566/Laravel10-AdminLte3-mixed">Admin User</a></strong>
+  </footer>
